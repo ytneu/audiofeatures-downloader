e22d21ce6e0fbcb27601d372625d2648554253a7
ytneu <ytneuu@gmail.com>
2019-03-26 17:01:08 +0100

    getAudioFeatures needs refactor

--- getAudioFeatures.py
+++ getAudioFeatures.py
@@ -1,7 +1,6 @@
 import argparse
 import warnings
-from get_audio_features.uris import uris_downloader
-from get_audio_features.audio_features import audio_features_downloader
+from get_audio_features import audio_features_downloader
 
 warnings.filterwarnings('ignore')
 
@@ -21,20 +20,16 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    uris = uris_downloader.Uris()
     features = audio_features_downloader.AudioFeatures()
 
     if args.spotify_key is not None:
-        uris.spotify_key = args.spotify_key
         features.spotify_key = args.spotify_key
 
     if args.spotify_secret is not None:
-        uris.spotify_secret = args.spotify_secret
         features.spotify_secret = args.spotify_secret
 
     spotify_key = args.spotify_key
     spotify_secret = args.spotify_secret
 
 
-    uris.get_uris(spotify_key, spotify_secret)
-    # features.save_features(spotify_key, spotify_secret)
+    features.save_features(spotify_key, spotify_secret)
